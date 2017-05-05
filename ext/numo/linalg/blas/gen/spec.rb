152bcc06256ecc86310924729d4faff75c9c0088
Masahiro TANAKA <masa16.tanaka@gmail.com>
2017-05-05 18:03:46 +0900

    avoid if statement

--- ext/numo/linalg/blas/gen/spec.rb
+++ ext/numo/linalg/blas/gen/spec.rb
@@ -10,16 +10,16 @@ when "z"
   real_char = "d"
 end
 
-if /[sd]/ =~ blas_char
+case blas_char
+when /[sd]/
   def_blas "?dot"
   def_blas "?nrm2"
   def_blas "?asum", "nrm2"
-end
-if "s" == blas_char
-  def_blas "dsdot", "dot"
-  def_blas "sdsdot"
-end
-if /[cz]/ =~ blas_char
+  if "s" == blas_char
+    def_blas "dsdot", "dot"
+    def_blas "sdsdot"
+  end
+when /[cz]/
   def_blas "?dotc", "dot", "?dotc_sub"
   def_blas "?dotu", "dot", "?dotu_sub"
   def_blas real_char+"?nrm2", "nrm2"
@@ -30,7 +30,8 @@ def_blas "?swap"
 def_blas "?copy"
 def_blas "?axpy"
 
-if /[sd]/ =~ blas_char
+case blas_char
+when /[sd]/
   def_blas "?rot"
   def_blas "?rotm"
 else
@@ -38,22 +39,21 @@ else
 end
 
 def_blas "?scal"
-if /[cz]/ =~ blas_char
+case blas_char
+when /[cz]/
   def_blas blas_char+real_char+"scal", "scal"
 end
 
 # level-2 blas
 def_blas "?gemv"
 def_blas "?trmv"
-if /[cz]/ =~ blas_char
-  def_blas "?hemv"
-end
 case blas_char
 when /[sd]/
   def_blas "?ger"
   def_blas "?syr"
   def_blas "?syr2"
 when /[cz]/
+  def_blas "?hemv"
   def_blas "?gerc", "ger"
   def_blas "?geru", "ger"
   def_blas "?her",  "syr"
@@ -63,7 +63,8 @@ end
 # level-3 blas
 def_blas "?gemm"
 def_blas "?symm"
-if /[cz]/ =~ blas_char
+case blas_char
+when /[cz]/
   def_blas "?hemm", "symm"
 end
 def_blas "?syrk"
