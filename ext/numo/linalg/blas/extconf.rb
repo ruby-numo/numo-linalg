d5fb660e38cdbe3e8e0ea40df5cfcb1045c0d1ae
Masahiro TANAKA <masa16.tanaka@gmail.com>
2018-03-23 18:27:28 +0900

    modify extconf.rb to use mkmf_linalg.rb

--- ext/numo/linalg/blas/extconf.rb
+++ ext/numo/linalg/blas/extconf.rb
@@ -1,35 +1,6 @@
 require 'mkmf'
 require 'numo/narray'
-require 'erb'
-
-ldirs = [
- dir_config("mkl")[1],
- dir_config("openblas")[1],
- dir_config("atlas")[1],
- dir_config("blas")[1],
- dir_config("lapack")[1],
-]
-bked = with_config("backend")
-
-FileUtils.mkdir_p "lib"
-open("lib/site_conf.rb","w"){|f| f.write "
-module Numo
-  module Linalg
-    BACKEND = #{bked.inspect}
-    MKL_LIBPATH = #{ldirs[0].inspect}
-    OPENBLAS_LIBPATH = #{ldirs[1].inspect}
-    ATLAS_LIBPATH = #{ldirs[2].inspect}
-    BLAS_LIBPATH = #{ldirs[3].inspect}
-    LAPACK_LIBPATH = #{ldirs[4].inspect}
-  end
-end"}
-
-$LOAD_PATH.each do |x|
-  if File.exist? File.join(x,'numo/numo/narray.h')
-    $INCFLAGS = "-I#{x}/numo " + $INCFLAGS
-    break
-  end
-end
+require_relative '../mkmf_linalg'
 
 srcs = %w(
 blas
@@ -40,11 +11,22 @@ blas_z
 )
 $objs = srcs.collect{|i| i+".o"}
 
-if !have_header('numo/narray.h')
+dir_config("narray")
+
+find_narray_h
+if !have_header("numo/narray.h")
   puts "
   Header numo/narray.h was not found. Give pathname as follows:
   % ruby extconf.rb --with-narray-include=narray_h_dir"
   exit(1)
+end
+
+if RUBY_PLATFORM =~ /cygwin|mingw/
+  find_libnarray_a
+  unless have_library("narray","nary_new")
+    puts "libnarray.a not found"
+    exit(1)
+  end
 end
 
 if have_header("dlfcn.h")
@@ -54,14 +36,6 @@ elsif have_header("windows.h")
   exit(1) unless have_func("LoadLibrary")
 end
 
-dep_path = File.join(__dir__, "depend")
-File.open(dep_path, "w") do |dep|
-  dep_erb_path = File.join(__dir__, "depend.erb")
-  File.open(dep_erb_path, "r") do |dep_erb|
-    erb = ERB.new(dep_erb.read)
-    erb.filename = dep_erb_path
-    dep.print(erb.result)
-  end
-end
-
+create_site_conf
+create_depend
 create_makefile('numo/linalg/blas')
--- ext/numo/linalg/lapack/extconf.rb
+++ ext/numo/linalg/lapack/extconf.rb
@@ -1,13 +1,6 @@
 require 'mkmf'
 require 'numo/narray'
-require 'erb'
-
-$LOAD_PATH.each do |x|
-  if File.exist? File.join(x,'numo/numo/narray.h')
-    $INCFLAGS = "-I#{x}/numo " + $INCFLAGS
-    break
-  end
-end
+require_relative '../mkmf_linalg'
 
 srcs = %w(
 lapack
@@ -18,11 +11,22 @@ lapack_z
 )
 $objs = srcs.collect{|i| i+".o"}
 
-if !have_header('numo/narray.h')
+dir_config("narray")
+
+find_narray_h
+if !have_header("numo/narray.h")
   puts "
   Header numo/narray.h was not found. Give pathname as follows:
   % ruby extconf.rb --with-narray-include=narray_h_dir"
   exit(1)
+end
+
+if RUBY_PLATFORM =~ /cygwin|mingw/
+  find_libnarray_a
+  unless have_library("narray","nary_new")
+    puts "libnarray.a not found"
+    exit(1)
+  end
 end
 
 if have_header("dlfcn.h")
@@ -32,14 +36,5 @@ elsif have_header("windows.h")
   exit(1) unless have_func("LoadLibrary")
 end
 
-dep_path = File.join(__dir__, "depend")
-File.open(dep_path, "w") do |dep|
-  dep_erb_path = File.join(__dir__, "depend.erb")
-  File.open(dep_erb_path, "r") do |dep_erb|
-    erb = ERB.new(dep_erb.read)
-    erb.filename = dep_erb_path
-    dep.print(erb.result)
-  end
-end
-
+create_depend
 create_makefile('numo/linalg/lapack')
