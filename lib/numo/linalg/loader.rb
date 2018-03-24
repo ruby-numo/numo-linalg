3c9471395a5f6b3f6feb1a148f36bd3bc8f52f6e
Masahiro TANAKA <masa16.tanaka@gmail.com>
2018-03-24 22:39:25 +0900

    try to load dynamic library with version suffix (#4)

--- lib/numo/linalg/loader.rb
+++ lib/numo/linalg/loader.rb
@@ -12,32 +12,41 @@ module Numo
         @@libs
       end
 
-      def load_mkl(path=nil,exc:true)
-        a = case path
-            when String; [path]
-            when Array ;  path
-            when NilClass; [MKL_LIBPATH,""].compact
-            else raise ArgumentError,"invalid path"
-            end
-        a.each do |d|
+      def dlopen(recv,base,dir=nil,ver=nil)
+        if dir && !dir.empty?
+          base = File.join(dir,base)
+        end
+        path = base+".#{EXT}"
+        begin
+          recv.send(:dlopen,path)
+        rescue =>e
+          if ver
+            path = base+".#{ver}"
+            recv.send(:dlopen,path)
+          else
+            raise e
+          end
+        end
+        path
+      end
+      private :dlopen
+
+      def load_mkl(*dirs,exc:true)
+        if dirs.empty?
+          dirs = [MKL_LIBPATH,""].compact
+        end
+        dirs.each do |d|
           if d.empty?
-            f_iomp5 = "libiomp5."+EXT
-            f_mkl_core = "libmkl_core."+EXT
-            f_intel_thread = "libmkl_intel_thread."+EXT
-            f_intel_lp64 = "libmkl_intel_lp64."+EXT
+            d = e = nil
           else
             e = d.sub(/\/mkl\//, "/")
-            f_iomp5 = File.join(e,"libiomp5."+EXT)
-            f_mkl_core = File.join(d,"libmkl_core."+EXT)
-            f_intel_thread = File.join(d,"libmkl_intel_thread."+EXT)
-            f_intel_lp64 = File.join(d,"libmkl_intel_lp64."+EXT)
           end
           begin
-            Fiddle.dlopen(f_iomp5)
-            Fiddle.dlopen(f_mkl_core)
-            Fiddle.dlopen(f_intel_thread)
-            Blas.dlopen(f_intel_lp64)
-            Lapack.dlopen(f_intel_lp64)
+            f_iomp5 = dlopen(Fiddle,"libiomp5",e)
+            f_mkl_core = dlopen(Fiddle,"libmkl_core",d)
+            f_intel_thread = dlopen(Fiddle,"libmkl_intel_thread",d)
+            f_intel_lp64 = dlopen(Blas,"libmkl_intel_lp64",d)
+            f_intel_lp64 = dlopen(Lapack,"libmkl_intel_lp64",d)
             @@libs = [ f_iomp5, f_mkl_core, f_intel_thread, f_intel_lp64 ]
             if $DEBUG
               $stderr.puts "Numo::Linalg: use #{f_intel_lp64}"
@@ -52,26 +61,24 @@ module Numo
         false
       end
 
-      def load_openblas(path=nil,exc:true)
-        a = case path
-            when String; [path]
-            when Array ;  path
-            when NilClass; [OPENBLAS_LIBPATH,""].compact
-            else raise ArgumentError,"invalid path"
-            end
-        a.each do |d|
-          %w[libopenblaso. libopenblasp. libopenblas.].each do |f|
-            f = File.join(d,f) if !d.empty?
-            f += EXT
+      def load_openblas(*dirs,exc:true)
+        if dirs.empty?
+          dirs = [OPENBLAS_LIBPATH,""].compact
+        end
+        dirs.each do |d|
+          %w[ libopenblaso
+              libopenblasp
+              libopenblas  ].each do |f|
             begin
-              Blas.dlopen(f)
-              Fiddle.dlopen(f)
+              f_openblas = dlopen(Blas,f,d,0)
+              f_openblas = dlopen(Fiddle,f,d,0)
+              f_lapacke = nil
               begin
-                Lapack.dlopen("liblapacke."+EXT)
+                f_lapacke = dlopen(Lapack,"liblapacke",d,3)
               rescue
-                Lapack.dlopen(f)
+                f_openblas = dlopen(Lapack,f,d,0)
               end
-              @@libs = [ f ]
+              @@libs = [ f_openblas, f_lapacke ].compact
               if $DEBUG
                 $stderr.puts "Numo::Linalg: use #{f}"
               end
@@ -86,25 +93,19 @@ module Numo
         false
       end
 
-      def load_atlas(path=nil,exc:true)
-        a = case path
-            when String; [path]
-            when Array ;  path
-            when NilClass; [ATLAS_LIBPATH,""].compact
-            else raise ArgumentError,"invalid path"
-            end
-        a.each do |d|
-          %w[libtatlas. libatlas. libsatlas.].each do |f|
-            f = File.join(d,f) if !d.empty?
-            f += EXT
+      def load_atlas(*dirs,exc:true)
+        if dirs.empty?
+          dirs = [ATLAS_LIBPATH,""].compact
+        end
+        dirs.each do |d|
+          %w[ libtatlas
+              libatlas
+              libsatlas ].each do |f|
             begin
-              Fiddle.dlopen(f)
-              Blas.dlopen(f)
-              l = LAPACK_LIBPATH
-              e = "liblapacke."+EXT
-              e = File.join(l,e) if l && !l.empty?
-              Lapack.dlopen(e)
-              @@libs = [ f, e ]
+              f_atlas = dlopen(Fiddle,f,d,3)
+              f_atlas = dlopen(Blas,f,d,3)
+              f_lapacke = dlopen(Lapack,"liblapacke",LAPACK_LIBPATH,3)
+              @@libs = [ f_atlas, f_lapacke ]
               if $DEBUG
                 $stderr.puts "Numo::Linalg: use #{f}"
               end
@@ -119,34 +120,20 @@ module Numo
         false
       end
 
-      def load_lapack(path=nil,exc:true)
-        a = case path
-            when String; [[path,path]]
-            when Array ;  [path]
-            when NilClass
-              b = BLAS_LIBPATH || LAPACK_LIBPATH
-              l = LAPACK_LIBPATH || BLAS_LIBPATH
-              (b ? [[b,l]] : []) + ["",""]
-            else raise ArgumentError,"invalid path"
-            end
-        a.each do |arg|
+      def load_lapack(*dirs,exc:true)
+        if dirs.empty?
+          b = BLAS_LIBPATH || LAPACK_LIBPATH
+          l = LAPACK_LIBPATH || BLAS_LIBPATH
+          dirs = (b ? [[b,l]] : []) + ["",""]
+        end
+        dirs.each do |arg|
           b,l = *arg
-          if b.empty?
-            f_blas = "libblas."+EXT
-            f_cblas = "libcblas."+EXT
-            f_lapack = "liblapack."+EXT
-            f_lapacke = "liblapacke."+EXT
-          else
-            f_blas = File.join(b,"libblas."+EXT)
-            f_cblas = File.join(b,"libcblas."+EXT)
-            f_lapack = File.join(l,"liblapack."+EXT)
-            f_lapacke = File.join(l,"liblapacke."+EXT)
-          end
+          l = b if l.nil?
           begin
-            Fiddle.dlopen(f_blas)
-            Fiddle.dlopen(f_lapack)
-            Blas.dlopen(f_cblas)
-            Lapack.dlopen(f_lapacke)
+            f_blas = dlopen(Fiddle,"libblas",b,3)
+            f_lapack = dlopen(Fiddle,"liblapack",l,3)
+            f_cblas = dlopen(Blas,"libcblas",b,3)
+            f_lapacke = dlopen(Lapack,"liblapacke",l,3)
             @@libs = [ f_blas, f_lapack, f_cblas, f_lapacke ]
             if $DEBUG
               $stderr.puts "Numo::Linalg: use #{f_blas} and #{f_lapack}"
